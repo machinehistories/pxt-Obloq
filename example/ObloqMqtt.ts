85bb2f7dba8c41ecc0e62e1034141dbced5b00bd
L-eeX <1035868977@qq.com>
2018-03-29 17:47:23 +0800

    obloq

---
+++ example/ObloqMqtt.ts
@@ -0,0 +1,17 @@
+Obloq.obloq_mqttCallbackUser( ({ myparam: message }) =>  {
+    basic.showString(message)
+})
+Obloq.Obloq_setup(
+"OPPO",
+"12345678",
+"ry-MOzFAPz",
+"H1zzdztAwz",
+"SkMaGKCDM",
+SerialPin.P1,
+SerialPin.P2
+)
+Obloq.Obloq_startConnect()
+basic.forever(() => {
+    Obloq.Obloq_sendMessage("mess")
+    basic.pause(5000)
+})
