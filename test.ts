a88fa9eaaa1fce7386be208ff2df6c8683c33319
L-eeX <1035868977@qq.com>
2018-07-24 11:03:18 +0800

    Version update -v0.0.1

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as a library
+Obloq.obloq_mqttCallbackUser( ({ myparam: message }) =>  {
+    basic.showString(message)
+})
+Obloq.Obloq_setup(
+"dfrobotYanfa",
+"hidfrobot",
+"HJZTNhw3fm",
+"HyGp4hD2zm",
+"rJq_biTXQ",
+SerialPin.P1,
+SerialPin.P2
+)
+Obloq.Obloq_startConnect(SERVERS.China)
+basic.forever(() => {
+    Obloq.Obloq_sendMessage("hello")
+    basic.pause(5000)
+})
